0f97dd657251152a1ce50b3a928f0c147829e172
Andri Sul <andri@ordent.co>
2023-05-12 16:48:17 +0700

    Setup API docs

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Auto Dealership API Docs",
+ *      description="Auto Dealership Swagger API Documentation",
+ *      @OA\Contact(
+ *          email="andrisul@outlook.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
+ * )
+ */
+class SwaggerController extends Controller
+{
+  //
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 
+/**
+ * @OA\Tag(
+ *     name="User",
+ *     description="API endpoints of user"
+ * )
+ */
 class UserController extends Controller
 {
-    // Get all users
+    /**
+     * Get list users (pagination)
+     * @OA\Get(
+     *     path="/api/user/list-page",
+     *     tags={"User"},
+     *     @OA\Response(response="200", description="OK")
+     * )
+     */
     public function listPage()
     {
         $users = User::all();
